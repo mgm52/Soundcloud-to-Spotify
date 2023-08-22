8b43ac8371d7bd98041af94ddca89c845cb44b94
Max McGuinness <mgm52@cam.ac.uk>
2023-08-22 20:43:58 +0100

    Build version

--- sc_to_sp.py
+++ sc_to_sp.py
@@ -56,118 +56,123 @@ def get_sp_result(sc_song, long_sc_song, indent=0):
 
 
 ##### SCRIPT #####
-source_txt = input("Please provide a path to a text file containing your Soundcloud likes (default: 'soundcloud_paste.txt'):\n")
-if source_txt == "":
-    source_txt = "soundcloud_paste.txt"
+try:
+    source_txt = input("Please provide a path to a text file containing your Soundcloud likes (default: 'soundcloud_paste.txt'):\n")
+    if source_txt == "":
+        source_txt = "soundcloud_paste.txt"
 
-# Read soundcloud_paste.txt
-soundcloud_paste = ""
-with open(source_txt, "r", encoding="utf-8") as f:
-    soundcloud_paste = f.read()
+    # Read soundcloud_paste.txt
+    soundcloud_paste = ""
+    with open(source_txt, "r", encoding="utf-8") as f:
+        soundcloud_paste = f.read()
 
-print(f"Read {len(soundcloud_paste)} characters from {source_txt}.")
+    print(f"Read {len(soundcloud_paste)} characters from {source_txt}.")
 
-# Take all lines between "\n\n" and "Legal ?"
-songs = soundcloud_paste.split("\n\n")[1].split("Legal ⁃")[0]
-songs = songs.split("\n")
+    # Take all lines between "\n\n" and "Legal ?"
+    songs = soundcloud_paste.split("\n\n")[1].split("Legal ⁃")[0]
+    songs = songs.split("\n")
 
-# Remove any lines consisting of only ׉
-songs = [line for line in songs if line.strip() != "׉" and line.strip() != ""]
-songcount = len(songs)/2
+    # Remove any lines consisting of only ׉
+    songs = [line for line in songs if line.strip() != "׉" and line.strip() != ""]
+    songcount = len(songs)/2
 
-# Pair into title and artist
-songs = list(zip(songs[::2], songs[1::2]))
-print(f"Filtered down to {len(songs)} songs.")
+    # Pair into title and artist
+    songs = list(zip(songs[::2], songs[1::2]))
+    print(f"Filtered down to {len(songs)} songs.")
 
-# Spotify developer credentials
-credentials = json.load(open("credentials.json", "r"))
-print(f"Loaded credentials for {credentials['username']}. Authenticating...")
+    # Spotify developer credentials
+    credentials = json.load(open("credentials.json", "r"))
+    print(f"Loaded credentials for {credentials['username']}. Authenticating...")
 
-# Authenticating
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id=credentials["client_id"],
-    client_secret=credentials["client_secret"],
-    redirect_uri=credentials["redirect_uri"],
-    scope="playlist-modify-public",
-    username=credentials["username"]))
-print("Authenticated successfully!")
+    # Authenticating
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+        client_id=credentials["client_id"],
+        client_secret=credentials["client_secret"],
+        redirect_uri=credentials["redirect_uri"],
+        scope="playlist-modify-public",
+        username=credentials["username"]))
+    print("Authenticated successfully!")
 
-# Creating a new playlist
-datenow = datetime.datetime.now()
-datenow_formatted = datenow.strftime("%H:%M %d/%m/%y")
-playlist = sp.user_playlist_create(user=credentials["username"], name=f"Soundcloud likes - x{songcount} - {datenow_formatted}")
-playlist_id = playlist["id"]
+    # Creating a new playlist
+    datenow = datetime.datetime.now()
+    datenow_formatted = datenow.strftime("%H:%M %d/%m/%y")
+    playlist = sp.user_playlist_create(user=credentials["username"], name=f"Soundcloud likes - x{songcount} - {datenow_formatted}")
+    playlist_id = playlist["id"]
 
-# Search for each song on Spotify
-print(f"\n\nFinding matches for {len(songs)} songs (ETA {round((35/60) * len(songs)/100, 1)} minutes):")
-uris, ambiguities, failures = [], [], []
-i = 1
-for sc_title, sc_artist in songs:
-    attempts = []
+    # Search for each song on Spotify
+    print(f"\n\nFinding matches for {len(songs)} songs (ETA {round((35/60) * len(songs)/100, 1)} minutes):")
+    uris, ambiguities, failures = [], [], []
+    i = 1
+    for sc_title, sc_artist in songs:
+        attempts = []
 
-    sc_song = f"{sc_title} - {sc_artist}"
-    song_reduced = cleanse_ignorechars(" ".join([word for word in sc_song.split() if "." not in word])).replace("Official", "")
-    
-    printable, long_printable, sc_song, sp_song, sp_uri, final_score = get_sp_result(sc_song, sc_song)
-    print(f"[{i}/{len(songs)}] {printable}")
-    attempts.append((sp_uri, final_score, printable, long_printable))
-
-    if final_score is None or final_score < 67:
-        printable, long_printable, sc_song, sp_song, sp_uri, final_score = get_sp_result(sc_title, sc_song, 2)
-        print(printable)
+        sc_song = f"{sc_title} - {sc_artist}"
+        song_reduced = cleanse_ignorechars(" ".join([word for word in sc_song.split() if "." not in word])).replace("Official", "")
+        
+        printable, long_printable, sc_song, sp_song, sp_uri, final_score = get_sp_result(sc_song, sc_song)
+        print(f"[{i}/{len(songs)}] {printable}")
         attempts.append((sp_uri, final_score, printable, long_printable))
 
-        printable, long_printable, sc_song, sp_song, sp_uri, final_score = get_sp_result(song_reduced, sc_song, 2)
-        print(printable)
-        attempts.append((sp_uri, final_score, printable, long_printable))
+        if final_score is None or final_score < 67:
+            printable, long_printable, sc_song, sp_song, sp_uri, final_score = get_sp_result(sc_title, sc_song, 2)
+            print(printable)
+            attempts.append((sp_uri, final_score, printable, long_printable))
 
-    # Remove any None entries from attempts
-    attempts = [attempt for attempt in attempts if attempt[0] is not None]
-    best_attempt = max(attempts, key=lambda x: x[1])
+            printable, long_printable, sc_song, sp_song, sp_uri, final_score = get_sp_result(song_reduced, sc_song, 2)
+            print(printable)
+            attempts.append((sp_uri, final_score, printable, long_printable))
 
-    if best_attempt[1] >= 67:
-        uris.append(best_attempt[0])
-    elif best_attempt[1] >= 1:
-        ambiguities.append(best_attempt)
-    else:
-        failures.append(best_attempt)
-    i+=1
+        # Remove any None entries from attempts
+        attempts = [attempt for attempt in attempts if attempt[0] is not None]
+        best_attempt = max(attempts, key=lambda x: x[1])
 
-# Manually resolve ambiguities
-print(f"\n{len(uris)} songs found, {len(failures)} not found, {len(ambiguities)} songs ambiguous:\n")
-i = 0
-for amb_uri, amb_match, amb_print, amb_long_print in ambiguities:
-    i += 1
-    print(f"{str(i)}/{len(ambiguities)}:" + amb_long_print)
-    ans = ""
-    while ans.lower() not in ["y", "n", "yall", "nall"]:
-        ans = input("Add this song? (y/n/yall/nall) ")
-    if ans.lower() == "y":
-        uris.append(amb_uri)
-    elif ans.lower() == "n":
-        failures.append((amb_uri, amb_match, amb_print, amb_long_print))
-    elif ans.lower() == "yall":
-        uris.append(amb_uri)
-        for amb_uri, amb_match, amb_print, amb_long_print in ambiguities[i:]:
+        if best_attempt[1] >= 67:
+            uris.append(best_attempt[0])
+        elif best_attempt[1] >= 1:
+            ambiguities.append(best_attempt)
+        else:
+            failures.append(best_attempt)
+        i+=1
+
+    # Manually resolve ambiguities
+    print(f"\n{len(uris)} songs found, {len(failures)} not found, {len(ambiguities)} songs ambiguous:\n")
+    i = 0
+    for amb_uri, amb_match, amb_print, amb_long_print in ambiguities:
+        i += 1
+        print(f"{str(i)}/{len(ambiguities)}:" + amb_long_print)
+        ans = ""
+        while ans.lower() not in ["y", "n", "yall", "nall"]:
+            ans = input("Add this song? (y/n/yall/nall) ")
+        if ans.lower() == "y":
             uris.append(amb_uri)
-        break
-    elif ans.lower() == "nall":
-        failures.append((amb_uri, amb_match, amb_print, amb_long_print))
-        for amb_uri, amb_match, amb_print, amb_long_print in ambiguities[i:]:
+        elif ans.lower() == "n":
             failures.append((amb_uri, amb_match, amb_print, amb_long_print))
-        break
+        elif ans.lower() == "yall":
+            uris.append(amb_uri)
+            for amb_uri, amb_match, amb_print, amb_long_print in ambiguities[i:]:
+                uris.append(amb_uri)
+            break
+        elif ans.lower() == "nall":
+            failures.append((amb_uri, amb_match, amb_print, amb_long_print))
+            for amb_uri, amb_match, amb_print, amb_long_print in ambiguities[i:]:
+                failures.append((amb_uri, amb_match, amb_print, amb_long_print))
+            break
 
-# Inform of failures
-print(f"\n{len(uris)} songs found, {len(failures)} songs not found:\n")
-for fail_uri, fail_match, fail_print, fail_long_print in failures:
-    print(fail_long_print)
+    # Inform of failures
+    print(f"\n{len(uris)} songs found, {len(failures)} songs not found:\n")
+    for fail_uri, fail_match, fail_print, fail_long_print in failures:
+        print(fail_long_print)
 
-# Adding songs to the playlist, 100 at a time
-for i in range(0, len(uris), 100):
-    sp.user_playlist_add_tracks(user=credentials['username'], playlist_id=playlist_id, tracks=uris[i:i+100])
+    # Adding songs to the playlist, 100 at a time
+    for i in range(0, len(uris), 100):
+        sp.user_playlist_add_tracks(user=credentials['username'], playlist_id=playlist_id, tracks=uris[i:i+100])
 
-# Account for leftover songs
-if len(uris) % 100 != 0:
-    sp.user_playlist_add_tracks(user=credentials['username'], playlist_id=playlist_id, tracks=uris[len(uris) - (len(uris) % 100):])
+    # Account for leftover songs
+    if len(uris) % 100 != 0:
+        sp.user_playlist_add_tracks(user=credentials['username'], playlist_id=playlist_id, tracks=uris[len(uris) - (len(uris) % 100):])
 
-print(f"Playlist of {len(uris)} songs created successfully, at https://open.spotify.com/playlist/{playlist_id}")
+    print(f"Playlist of {len(uris)} songs created successfully, at https://open.spotify.com/playlist/{playlist_id}")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+input("Press enter to exit.")
